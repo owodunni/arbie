472ae8e3dc1b03b5d1f803dfa9fa3d0ea7a1bb2f
Alexander Poole <alex.o.poole@gmail.com>
2020-07-11 19:12:22 +0200

    Generate xml coverage report and upload to coverage

--- dodo.py
+++ dodo.py
@@ -7,7 +7,9 @@ project_dir = os.getcwd()
 build_dir = f"{project_dir}/build"
 project_name = "Arbie"
 sorce_dir = f"{project_dir}/{project_name}"
+
 test_dir = f"{sorce_dir}/tests"
+test_result_dir = f"{build_dir}/tests"
 
 pip = "pip"
 pip_dir = f"{project_dir}/{pip}"
@@ -16,14 +18,12 @@ actions = "actions"
 
 
 def task_test():
-    test_result_dir = f"{build_dir}/tests"
-
     return {
         actions:
             [f"pytest -v --pyargs {project_name} "  # noqa: WPS221
-             f"--cov-report html:{test_result_dir}/cov "
-             f"--cov={sorce_dir} {test_dir} "
-             f"--junit-xml={test_result_dir}/test_results.xml",
+             f"--cov-report html:{test_result_dir}/cov/html "
+             f"--cov-report xml:{test_result_dir}/cov/coverage.xml "
+             f"--cov={sorce_dir} {test_dir} ",
              ],
     }
 
@@ -46,3 +46,17 @@ def task_pip():
 
 def task_pip_dev():
     return {actions: [f"{pip} install -r {pip_dir}/requirements-dev.txt"]}
+
+
+def task_codecov():
+    """Upload coverage report to codecov.
+
+    This task requires that CODECOV_TOKEN env variable is set.
+    Read more here: https://docs.codecov.io/v4.3.0/docs/about-the-codecov-bash-uploader
+    """
+    return {
+        actions:
+            [f"tools/codecov.sh -f "
+                f"{test_result_dir}/cov/coverage.xml",
+             ],
+    }
