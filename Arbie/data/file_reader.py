7b6c12404330375cd7343d07ed98e09113053e53
Alexander Poole <alex.o.poole@gmail.com>
2020-07-19 19:18:40 +0200

    Prefer single quotes over double

--- Arbie/data/file_reader.py
+++ Arbie/data/file_reader.py
@@ -3,7 +3,7 @@ from pkg_resources import resource_string
 
 
 def read_resource(filename, path=None):
-    resource_path = "arbie.resources"
+    resource_path = 'arbie.resources'
     if path:
-        resource_path = f"{resource_path}.{path}"
-    return resource_string(resource_path, filename).decode("utf-8")
+        resource_path = f'{resource_path}.{path}'
+    return resource_string(resource_path, filename).decode('utf-8')
