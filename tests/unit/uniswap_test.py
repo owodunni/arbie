1b11087755909cb68ff54cc578e588c94288e47c
Alexander Poole <alex.o.poole@gmail.com>
2020-07-22 22:14:28 +0200

    Import uniswap

--- tests/unit/uniswap_test.py
+++ tests/unit/uniswap_test.py
@@ -1,10 +1,12 @@
 """Some tests."""
 
 import unittest
+from Arbie.Contracts.uniswap import Uniswap
 
 
 class TestReadResources(unittest.TestCase):
     """Test file reader."""
 
     def test_make_trade(self):
+        uni = Uniswap()
         self.assertFalse(False)
