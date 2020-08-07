9f328c821328a07717145f83fda5497d71c6be98
Alexander Poole <alex.o.poole@gmail.com>
2020-08-07 07:15:20 +0000

    Remove overloaded operators from Variables
    Add test case for variable
    
    Add tests for bad trade opertunities1

--- tests/unit/Actions/arbitrage_test.py
+++ tests/unit/Actions/arbitrage_test.py
@@ -2,8 +2,10 @@
 
 import unittest
 
-from Arbie.Actions.amm import Amm
-from Arbie.Actions.arbitrage import TradeOpertunity, find_arbitrage
+from Arbie.Actions.amm import Amm, Token
+from Arbie.Actions.arbitrage import (TradeOpertunity,
+                                     calculate_optimal_arbitrage,
+                                     find_arbitrage)
 
 from .amm_test import dai, eth, tokens  # noqa: WPS300
 
@@ -24,3 +26,36 @@ class TestArbitrage(unittest.TestCase):
         trade = TradeOpertunity([pool1, pool2], dai, eth)
 
         self.assertAlmostEqual(find_arbitrage(trade), 27.8547574719045)  # noqa: WPS432
+
+    def test_find_arbitrage_no_opertunity(self):
+        pool1 = Amm(tokens, [400, 1], [0.9, 0.1])
+        pool2 = Amm(tokens, [410, 1], [0.1, 0.9])
+        trade = TradeOpertunity([pool1, pool2], eth, dai)
+
+        with self.assertRaises(ValueError):
+            find_arbitrage(trade)
+
+    def test_calc_optimal_arbitrage_no_opertunity(self):
+        pool1 = Amm(tokens, [400, 1], [0.9, 0.1])
+        pool2 = Amm(tokens, [410, 1], [0.1, 0.9])
+        trade = TradeOpertunity([pool1, pool2], eth, dai)
+
+        with self.assertRaises(ValueError) as e:
+            calculate_optimal_arbitrage(trade)
+            self.assertEqual('No arbitrage opertunity found.', e.message)
+
+    def test_find_arbitrage_wrong_len(self):
+        pool1 = Amm(tokens, [400, 1], [0.9, 0.1])
+        pool2 = Amm(tokens, [410, 1], [0.1, 0.9])
+        trade = TradeOpertunity([pool1, pool2, pool1], dai, eth)
+
+        with self.assertRaises(ValueError):
+            find_arbitrage(trade)
+
+    def test_find_arbitrage_wrong_token(self):
+        pool1 = Amm(tokens, [400, 1], [0.9, 0.1])
+        pool2 = Amm(tokens, [410, 1], [0.1, 0.9])
+        trade = TradeOpertunity([pool1, pool2], dai, Token('sai'))
+
+        with self.assertRaises(ValueError):
+            find_arbitrage(trade)
