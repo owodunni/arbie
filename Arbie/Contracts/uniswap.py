f96b8dfc8c22cbd7be6f7894bb2179def121cb47
Alexander Poole <alex.o.poole@gmail.com>
2020-08-07 10:56:52 +0000

    Add classes for loading uniswap contracts
    Export functions from the Contracts module

--- Arbie/Contracts/uniswap.py
+++ Arbie/Contracts/uniswap.py
@@ -1,5 +1,20 @@
 """Utility functions for interacting with Uniswap."""
 
+from web3 import Web3
 
-class Uniswap(object):
-    """Utility functions for trading tokens from Uniswap."""
+from Arbie.Contracts.contract import Address, Contract, Network
+
+
+class Factory(Contract):
+
+    def __init__(self, w3: Web3, network=Network.mainnet):
+        abi = self._read_resource('uniswap', 'factory_v2_abi.json')
+        self.address = self._get_address('uniswap', 'factory_v2', network)
+        self.contract = w3.eth.contract(address=self.address.value, abi=abi)
+
+
+class Pair(Contract):
+
+    def __init__(self, w3: Web3, address: Address):
+        abi = self._read_resource('uniswap', 'pair_abi.json')
+        self.contract = w3.eth.contract(address=address.value, abi=abi)
