0aad9a3668c3da74f5df0b1d1b33f018d6411a0c
Alexander Poole <alex.o.poole@gmail.com>
2020-08-10 05:39:16 +0000

    Add test for creating new bpool

--- Arbie/Contracts/balancer.py
+++ Arbie/Contracts/balancer.py
@@ -6,3 +6,11 @@ from Arbie.Contracts import Contract
 class PoolFactory(Contract):
     name = 'pool_factory'
     protocol = 'balancer'
+
+    def new_bpool(self):
+        transaction = self.contract.functions.newBPool()
+
+        tx_hash = transaction.transact({
+            'from': self.w3.eth.accounts[0],
+        })
+        self.w3.eth.waitForTransactionReceipt(tx_hash, 180)  # noqa: WPS432
--- tests/unit/Contracts/balancer_test.py
+++ tests/unit/Contracts/balancer_test.py
@@ -4,6 +4,9 @@ from Arbie.Contracts.balancer import PoolFactory
 
 from .web3_helper_test import *  # noqa: F403, WPS300, WPS347, F401
 
+@pytest.fixture
+def pool_factory(deploy_address, w3) -> PoolFactory:
+    return ContractFactory(w3, PoolFactory).deploy_contract(deploy_address)
 
-def test_create_pool_factory(deploy_address, w3):
-    ContractFactory(w3, PoolFactory).deploy_contract(deploy_address)
+def test_create_new_pool(pool_factory):
+    pool_factory.new_bpool()
