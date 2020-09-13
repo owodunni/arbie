248ea8ceff936ee5b0b2d005e9aa48d1f4ec9a7c
Alexander Poole <alex.o.poole@gmail.com>
2020-09-13 20:30:40 +0200

    Run tests that require blockchains agains ganache-cli
    Set contract gas limit
    
    Require tests to have a running eth chain before execution
    
    Start a instance of Ganache-cli on CI server before running tests
    At first we considered haveing Ganache inside our Python
    docker container. However seperations of conserns is key.
    
    Try to sleep and see if that fixes tests
    
    Run tests in docker-compose
    
    No success yet look at this thread to how to get to goal
    https://stackoverflow.com/questions/40907954/terminate-docker-compose-when-test-container-finishes
    
    Run ganche chain with docker-compose
    
    Format build.gradle

--- tests/unit/Contracts/web3_helper_test.py
+++ tests/unit/Contracts/web3_helper_test.py
@@ -1,31 +1,16 @@
 """Help module for web3 tests."""
 import pytest
-from eth_tester import PyEVMBackend
-from web3 import EthereumTesterProvider, Web3
+from web3 import Web3
 
-from Arbie.Contracts import Address
-
-
-@pytest.fixture
-def tester_provider():
-    genesis_overrides = {'gas_limit': 4881400}
-    custom_genesis_params = PyEVMBackend._generate_genesis_params(overrides=genesis_overrides)  # noqa: WPS437
-    pyevm_backend = PyEVMBackend(genesis_parameters=custom_genesis_params)
-
-    return EthereumTesterProvider(pyevm_backend)
+from Arbie.Contracts.contract import Address
 
 
 @pytest.fixture
-def eth_tester(tester_provider):
-    return tester_provider.ethereum_tester
+def w3():
+    return Web3(Web3.HTTPProvider('http://ganache:7545'))
 
 
 @pytest.fixture
-def w3(tester_provider):
-    return Web3(tester_provider)
-
-
-@pytest.fixture
-def deploy_address(eth_tester) -> Address:
-    deploy_address = eth_tester.get_accounts()[0]
+def deploy_address(w3) -> Address:
+    deploy_address = w3.eth.accounts[0]
     return Address(deploy_address)
