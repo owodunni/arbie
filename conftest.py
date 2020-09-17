16ba0b331c39565310feaa6d1b6446e4bc3b3839
Alexander Poole <alex.o.poole@gmail.com>
2020-09-17 19:48:02 +0200

    Move test fixtures to conftest

--- conftest.py
+++ conftest.py
@@ -1,2 +1,25 @@
+"""Help module for web3 tests."""
+import pytest
+from web3 import Web3
+
+from Arbie.Contracts.contract import Address
+
+
 def pytest_addoption(parser):
     parser.addoption('--web3_server', action='store', default='http://127.0.0.1:7545')
+
+
+@pytest.fixture
+def web3_server(request):
+    return request.config.getoption('--web3_server')
+
+
+@pytest.fixture
+def w3(web3_server):
+    return Web3(Web3.HTTPProvider(web3_server))
+
+
+@pytest.fixture
+def deploy_address(w3) -> Address:
+    deploy_address = w3.eth.accounts[0]
+    return Address(deploy_address)
--- tests/unit/Contracts/balancer_test.py
+++ tests/unit/Contracts/balancer_test.py
@@ -4,8 +4,6 @@ import pytest
 from Arbie.Contracts import ContractFactory
 from Arbie.Contracts.balancer import PoolFactory
 
-from .web3_helper_test import *  # noqa: F403, WPS300, WPS347, F401
-
 
 @pytest.fixture
 def pool_factory(deploy_address, w3) -> PoolFactory:
