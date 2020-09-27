1a104202784008135d7eb7312bf090a47cbf4eb5
Alexander Poole <alex.o.poole@gmail.com>
2020-09-27 11:51:10 +0200

    Refactor uniswap tests

--- tests/unit/Contracts/uniswap_test.py
+++ tests/unit/Contracts/uniswap_test.py
@@ -3,7 +3,7 @@ import pytest
 
 from Arbie.Contracts import ContractFactory
 from Arbie.Contracts.tokens import GenericToken
-from Arbie.Contracts.uniswap import Factory
+from Arbie.Contracts.uniswap import Factory, Pair
 
 
 @pytest.fixture
@@ -26,27 +26,33 @@ def weth(deploy_address, token_factory) -> GenericToken:
     return token_factory.deploy_contract(deploy_address, 100, 'Weth', 18, 'WETH')  # noqa: WPS432
 
 
+@pytest.fixture
+def factory_with_pair(factory, dai, weth) -> Factory:
+    assert factory.create_pair(dai, weth)
+    return factory
+
+
 def test_get_all_pairs_length(factory):
     assert factory.all_pairs_length() == 0
 
 
-def test_create_pair(factory, dai, weth):
-    factory.create_pair(dai, weth)
-    assert factory.all_pairs_length() == 1
+def test_create_pair(factory_with_pair):
+    assert factory_with_pair.all_pairs_length() == 1
 
 
-def test_get_all_pairs(factory, dai, weth):
-    factory.create_pair(dai, weth)
-    assert len(factory.all_pairs()) == 1
+def test_get_all_pairs(factory_with_pair):
+    assert len(factory_with_pair.all_pairs()) == 1
 
 
-def test_get_k(factory, dai, weth):
-    factory.create_pair(dai, weth)
-    pairs = factory.all_pairs()
-    assert pairs[0].get_k_last() == 0
+@pytest.fixture
+def pair(factory_with_pair) -> Pair:
+    pairs = factory_with_pair.all_pairs()
+    return pairs[0]
 
 
-def test_get_weights(factory, dai, weth):
-    factory.create_pair(dai, weth)
-    pairs = factory.all_pairs()
-    assert pairs[0].get_reserves() == [0, 0, 0]
+def test_get_k(pair):
+    assert pair.get_k_last() == 0
+
+
+def test_get_weights(pair):
+    assert pair.get_reserves() == [0, 0, 0]
