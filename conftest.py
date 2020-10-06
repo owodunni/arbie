c7bff6f84950b25e83a75554cc5dc43e48ecd622
Alexander Poole <alex.o.poole@gmail.com>
2020-10-06 07:31:19 +0200

    Seperate unit tests and system tests

--- conftest.py
+++ conftest.py
@@ -1,52 +1,6 @@
 """Help module for web3 tests."""
 import pytest
-from web3 import Web3
 
-from Arbie.Contracts.contract import Address, ContractFactory
-from Arbie.Contracts.tokens import GenericToken
 
 def pytest_addoption(parser):
     parser.addoption('--web3_server', action='store', default='http://127.0.0.1:7545')
-
-
-@pytest.fixture
-def web3_server(request):
-    return request.config.getoption('--web3_server')
-
-
-@pytest.fixture
-def w3(web3_server):
-    return Web3(Web3.HTTPProvider(web3_server))
-
-
-@pytest.fixture
-def deploy_address(w3) -> Address:
-    deploy_address = w3.eth.accounts[0]
-    return Address(deploy_address)
-
-
-@pytest.fixture
-def dummy_address(w3) -> Address:
-    address = w3.eth.accounts[1]
-    return Address(address)
-
-
-@pytest.fixture
-def token_factory(w3) -> ContractFactory:
-    return ContractFactory(w3, GenericToken)
-
-
-@pytest.fixture
-def dai(deploy_address, token_factory) -> GenericToken:
-    token = token_factory.deploy_contract(
-        deploy_address, 10000, 'Dai', 18, 'DAI')
-    token.approve_owner()
-    return token
-
-
-@pytest.fixture
-def weth(deploy_address, token_factory) -> GenericToken:
-    token = token_factory.deploy_contract(
-        deploy_address, 10000, 'Weth', 18, 'WETH')
-    token.approve_owner()
-    return token
--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 """Packaging logic for arbie."""
 from setuptools import find_packages, setup
-
+import os
 
 def read(path):
     with open(path) as req:
         return req.read().splitlines()
 
+def package_files(directory, file_filter):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            if any(substring in filename for substring in file_filter):
+                paths.append(os.path.join('..', path, filename))
+    return paths
+
+extra_files = package_files('Arbie/resources', ['.json'])
 
 setup(
-    packages=find_packages(exclude=['tests', 'unit']),
-    package_data={'': ['resources/*']},
+    packages=find_packages(exclude=['tests', 'unit', 'system']),
+    package_data={'': extra_files},
     install_requires=read('pip/requirements.txt'),
     tests_require=read('pip/requirements-dev.txt'),
 )
