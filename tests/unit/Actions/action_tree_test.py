03a9c76d24638929677a664d77114cb18bf0b0e6
Alexander Poole <alex.o.poole@gmail.com>
2020-10-21 21:12:56 +0200

    Add tests for creating ActionTree

---
+++ tests/unit/Actions/action_tree_test.py
@@ -0,0 +1,51 @@
+"""Unittest of ActionTree."""
+
+import pytest
+import yaml
+
+from Arbie.Actions import Action, ActionTree, Store
+
+
+class DummyAction(Action):
+    """
+    Dummy description for dummy action.
+
+    [Settings]
+    input:
+    output:
+    """
+
+
+store = Store()
+
+
+class TestActionTree(object):
+
+    def test_create(self):
+        tree = ActionTree.create({'PathFinder': {}}, store)
+        assert len(tree.actions) == 1
+
+    def test_create_bad_arg(self):
+        config = """
+        PathFinder:
+            input:
+                non_existing: []
+        """
+        with pytest.raises(ValueError):
+            ActionTree.create(yaml.safe_load(config), store)
+
+    def test_create_bad_action(self):
+        with pytest.raises(ValueError):
+            ActionTree.create({'NonExistingAction': {}}, store)
+
+    def test_create_dummy_action(self):
+        config = """
+        PathFinder:
+        DummyAction:
+        """
+
+        tree = ActionTree.create(
+            yaml.safe_load(config),
+            store,
+            [('DummyAction', DummyAction)])
+        assert len(tree.actions) == 2
