cdac26518c27899961ada1419aee9ebd58e75928
Alexander Poole <alex.o.poole@gmail.com>
2020-10-22 08:12:43 +0200

    Add Action for finding trades

--- Arbie/Actions/arbitrage.py
+++ Arbie/Actions/arbitrage.py
@@ -1,24 +1,26 @@
 """arbitrage can be used to find to arbitrage opertunity between two Pools."""
 
-from typing import List, NewType
+from typing import Tuple
 
 from sympy import nsolve, symbols
 
-from Arbie.Variables import ArbitrageOpportunity, Balance, Pools, Token
+from Arbie.Variables import ArbitrageOpportunity, Balance
 from Arbie.Actions import Action
 
 x = symbols('x')
 
 
-def find_arbitrage(trades: ArbitrageOpportunity) -> Balance:
+def find_arbitrage(trades: ArbitrageOpportunity) -> Tuple[float, float]:
     if len(trades) < 2:
         raise ValueError('Can only found arbitrage opportunity between two or more pools')
 
     if not token_in_pools(trades):
         raise ValueError('Tokens does not exist in pools')
-    
-    profit = calculate_optimal_arbitrage(trades)
-    return Balance(trades[0].token_in, profit)
+
+    trade_input = calculate_optimal_arbitrage(trades)
+    profit = calculate_profit(trades, trade_input)
+
+    return trade_input, profit
 
 
 def token_in_pools(trades: ArbitrageOpportunity) -> bool:
@@ -27,6 +29,19 @@ def token_in_pools(trades: ArbitrageOpportunity) -> bool:
         if trade.token_in not in pool.tokens or trade.token_out not in pool.tokens:
             return False
     return True
+
+
+def trade_expr(trades: ArbitrageOpportunity):
+    first_trade = trades[0]
+    expr = first_trade.pool.out_given_in_expr(
+        first_trade.token_in,
+        first_trade.token_out)
+
+    for trade in trades[1:]:
+        inner_expr = trade.pool.out_given_in_expr(trade.token_in, trade.token_out)
+        expr = inner_expr.subs(x, expr)
+
+    return expr
 
 
 def arbitrage_expr(trades: ArbitrageOpportunity):
@@ -49,12 +64,17 @@ def arbitrage_diff_expr(trade: ArbitrageOpportunity):
 def calculate_optimal_arbitrage(trade: ArbitrageOpportunity) -> float:
     sol = nsolve(arbitrage_diff_expr(trade), 0)
     if sol <= 0:
-        raise  AssertionError('No arbitrage opportunity found.')
+        raise AssertionError('No arbitrage opportunity found.')
     return sol
 
 
+def calculate_profit(trade: ArbitrageOpportunity, value) -> float:
+    expr = trade_expr(trade)
+    return expr.subs(x, value) - value
+
+
 class Arbitrage(Action):
-    """Find optimal arbitrage opertunity for a list sorted trades.
+    """Find optimal arbitrage opportunity for a list sorted trades.
 
     Remove all trades that are not profitable.
 
@@ -63,17 +83,18 @@ class Arbitrage(Action):
         trades: all_trades
     output:
         out_trades: filtered_trades
-        out_balance: trade_balance
     """
 
     def on_next(self, data):
-        out_trades = []
-        out_balance = []
-        for trade in data.trades()
+        trades = []
+        for arbitrage_opportunity in data.trades():
             try:
-                out_balance.append(find_arbitrage(trade))
-                out_trades.append(trade)
+                amount_in, profit = find_arbitrage(arbitrage_opportunity)
+                arbitrage_opportunity.amount_in = amount_in
+                arbitrage_opportunity.profit = profit
+                trades.append(arbitrage_opportunity)
+            except AssertionError:
+                pass
 
-        
-                
-            
+        sorted_trade = sorted(trades, key=lambda trade: trade.profit, reverse=True)
+        data.out_trades(sorted_trade)
--- tests/unit/Actions/arbitrage_test.py
+++ tests/unit/Actions/arbitrage_test.py
@@ -2,31 +2,38 @@
 
 import pytest
 
-from Arbie.Variables import Pool, Token, Trade
+from Arbie.Variables import Pool, Token, Trade, ArbitrageOpportunity
 from Arbie.Actions.arbitrage import (calculate_optimal_arbitrage,
-                                     find_arbitrage)
+                                     find_arbitrage, Arbitrage)
+from Arbie.Actions import ActionTree, Store
 
 dai = Token('dai', 300.0)
 eth = Token('eth', 1)
 tokens = [dai, eth]
 
 
+@pytest.fixture
+def trade1():
+    pool1 = Pool(tokens, [400, 1], [0.5, 0.5])
+    pool2 = Pool(tokens, [410, 1], [0.5, 0.5])
+    return ArbitrageOpportunity([Trade(pool1, dai, eth), Trade(pool2, eth, dai)])
+
+
+@pytest.fixture
+def trade2():
+    pool1 = Pool(tokens, [400, 1], [0.9, 0.1])
+    pool2 = Pool(tokens, [410, 1], [0.1, 0.9])
+    return ArbitrageOpportunity([Trade(pool1, dai, eth), Trade(pool2, eth, dai)])
+
+
 class TestArbitrage(object):
     """Test Arbitrage."""
 
-    def test_find_arbitrage(self):
-        pool1 = Pool(tokens, [400, 1], [0.5, 0.5])
-        pool2 = Pool(tokens, [410, 1], [0.5, 0.5])
-        trade = [Trade(pool1, dai, eth), Trade(pool2, eth, dai)]
+    def test_find_arbitrage(self, trade1):
+        assert find_arbitrage(trade1).value == pytest.approx(2.48456731316587)  # noqa: WPS432
 
-        assert find_arbitrage(trade).value == pytest.approx(2.48456731316587)  # noqa: WPS432
-
-    def test_find_arbitrage_unbalanced(self):
-        pool1 = Pool(tokens, [400, 1], [0.9, 0.1])
-        pool2 = Pool(tokens, [410, 1], [0.1, 0.9])
-        trade = [Trade(pool1, dai, eth), Trade(pool2, eth, dai)]
-
-        assert find_arbitrage(trade).value == pytest.approx(27.8547574719045)  # noqa: WPS432
+    def test_find_arbitrage_unbalanced(self, trade2):
+        assert find_arbitrage(trade2).value == pytest.approx(27.8547574719045)  # noqa: WPS432
 
     def test_find_arbitrage_no_opportunity(self):
         pool1 = Pool(tokens, [400, 1], [0.9, 0.1])
@@ -34,7 +41,7 @@ class TestArbitrage(object):
         trade = [Trade(pool1, eth, dai), Trade(pool2, dai, eth)]
 
         with pytest.raises(ValueError):
-            find_arbitrage(trade)
+            find_arbitrage(ArbitrageOpportunity(trade))
 
     def test_calc_optimal_arbitrage_no_opportunity(self):
         pool1 = Pool(tokens, [400, 1], [0.9, 0.1])
@@ -42,7 +49,7 @@ class TestArbitrage(object):
         trade = [Trade(pool1, eth, dai), Trade(pool2, dai, eth)]
 
         with pytest.raises(ValueError) as e:
-            calculate_optimal_arbitrage(trade)
+            calculate_optimal_arbitrage(ArbitrageOpportunity(trade))
             assert e.message == 'No arbitrage opportunity found.'
 
     def test_find_arbitrage_wrong_token(self):
@@ -52,4 +59,16 @@ class TestArbitrage(object):
         trade = [Trade(pool1, dai, sai), Trade(pool2, sai, dai)]
 
         with pytest.raises(ValueError):
-            find_arbitrage(trade)
+            find_arbitrage(ArbitrageOpportunity(trade))
+
+
+class TestArbitrageAction(object):
+
+    def test_on_next(self, trade1, trade2):
+        store = Store()
+        store.add('all_trades', [trade1, trade2])
+        tree = ActionTree(store)
+        tree.add_action(Arbitrage())
+        tree.run()
+
+        assert len(store.get('filtered_trades')) == 2
