67d13d8bbdc9700f341cfa4abf2a9786275e1e07
Alexander Poole <alex.o.poole@gmail.com>
2020-11-01 14:16:20 +0100

    Add example using arbie from Jupyter

--- Arbie/__main__.py
+++ Arbie/__main__.py
@@ -37,4 +37,5 @@ def main(argv=None):
     config = None
     with open(path_to_file, "r") as file:
         config = yaml.safe_load(file)
-    App(config).run()
+    app = App(config)
+    app.run()
