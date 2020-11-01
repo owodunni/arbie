8ddc89f11464852a96b4a118992c5b1c54768a8e
Alexander Poole <alex.o.poole@gmail.com>
2020-11-01 13:46:57 +0100

    Add tests for circuit breaker

---
+++ tests/unit/Contracts/circuit_breaker_test.py
@@ -0,0 +1,12 @@
+"""Unit tests for Circuit breaker."""
+import pytest
+
+from Arbie.Contracts.circuit_breaker import CircuitBreaker
+
+def raise_exception():
+    raise AssertionError()
+
+def test_safe_call():
+    breaker = CircuitBreaker(1, 0, raise_exception)
+    with pytest.raises(AssertionError):
+        breaker.safe_call()
