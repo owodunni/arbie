e79d5adaf51ad2ad9757c5ce6e1b4ab3f52a71d9
Alexander Poole <alex.o.poole@gmail.com>
2020-11-07 20:58:27 +0100

    Release v0.3.1

--- Arbie/__init__.py
+++ Arbie/__init__.py
@@ -6,7 +6,7 @@ from Arbie.exception import (  # noqa: F401
     PoolValueError,
 )
 
-__version__ = "0.3.0"  # noqa: WPS410
+__version__ = "0.3.1"  # noqa: WPS410
 __version_info__ = tuple(
     int(i) for i in __version__.split(".") if i.isdigit()
 )  # noqa: WPS221
