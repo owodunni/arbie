bfc0f9d47db4b1358644f6600ae66a1df28becdf
Alexander Poole <alex.o.poole@gmail.com>
2020-11-25 17:58:07 +0100

    refactor(async): Make async map work on chunks of promises

--- Arbie/async_helpers.py
+++ Arbie/async_helpers.py
@@ -6,9 +6,23 @@ from concurrent.futures import ThreadPoolExecutor
 thread_pool = ThreadPoolExecutor(max_workers=15)  # noqa: WPS432
 
 
-async def async_map(function, iterator):
-    tasks = [function(i) for i in iterator]
+def chunked(seq, chunk_size):
+    yield from (
+        seq[index : index + chunk_size]
+        for index in range(0, len(seq), chunk_size)  # noqa: WPS518
+    )
+
+
+async def async_map_chunk(function, seq):
+    tasks = [function(i) for i in seq]
     return await asyncio.gather(*tasks)
+
+
+async def async_map(function, seq, chunk_size=10):
+    res = []
+    for chunk in chunked(seq, chunk_size):
+        res.extend(await async_map_chunk(function, chunk))
+    return res
 
 
 async def run_async(function, *args):
