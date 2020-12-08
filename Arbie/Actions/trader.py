9328ce2eadad262f19b71c5bb0c7fc773b9e28e0
Alexander Poole <alex.o.poole@gmail.com>
2020-12-08 08:04:01 +0100

    refactor(trader): Add method for checking total balance

--- Arbie/Actions/trader.py
+++ Arbie/Actions/trader.py
@@ -4,43 +4,9 @@ import logging
 
 from Arbie import TransactionError
 from Arbie.Actions import Action
-from Arbie.Contracts import GenericToken
 from Arbie.Variables import BigNumber, PoolType
 
 logger = logging.getLogger()
-
-
-def _is_trade_uni(trade):
-    for pool in trade.pools:
-        if pool.pool_type is not PoolType.uniswap:
-            return False
-    return True
-
-
-async def get_balance(weth: GenericToken, web3, trader_address):
-    balance_pre = await weth.balance_of(trader_address)
-    account_balance = BigNumber.from_value(web3.eth.getBalance(trader_address))
-    return balance_pre.to_number() + account_balance.to_number()
-
-
-def _perform_trade(trade, arbie, address, min_profit):
-    if not _is_trade_uni(trade):
-        return False
-
-    amount_out = arbie.check_out_given_in(trade)
-    if amount_out - trade.amount_in > min_profit:
-        logger.info(f"Executing trade with return: {amount_out}, trade: {trade}")
-        return arbie.swap(trade, address)
-    return False
-
-
-def perform_trade(data, trader_address):
-    arbie = data.arbie()
-    min_profit = data.min_profit()
-    for trade in data.trades():
-        if _perform_trade(trade, arbie, trader_address, min_profit):
-            return True
-    return False
 
 
 class BalanceChecker(object):
@@ -52,6 +18,9 @@ class BalanceChecker(object):
         amount_weth = await self.weth.balance_of(address)
         amount_eth = BigNumber.from_value(self.web3.eth.getBalance(address))
         return amount_eth.to_number(), amount_weth.to_number()
+
+    async def check_total(self, address):
+        return sum(await self.check(address))
 
     async def check_and_convert(
         self, trader_address, min_eth, min_weth, max_weth, gas_cost
@@ -126,6 +95,33 @@ class SetUpTrader(Action):
         data.balance_weth(amount_weth)
 
 
+def _is_trade_uni(trade):
+    for pool in trade.pools:
+        if pool.pool_type is not PoolType.uniswap:
+            return False
+    return True
+
+
+def _perform_trade(trade, arbie, address, min_profit):
+    if not _is_trade_uni(trade):
+        return False
+
+    amount_out = arbie.check_out_given_in(trade)
+    if amount_out - trade.amount_in > min_profit:
+        logger.info(f"Executing trade with return: {amount_out}, trade: {trade}")
+        return arbie.swap(trade, address)
+    return False
+
+
+def perform_trade(data, trader_address):
+    arbie = data.arbie()
+    min_profit = data.min_profit()
+    for trade in data.trades():
+        if _perform_trade(trade, arbie, trader_address, min_profit):
+            return True
+    return False
+
+
 class Trader(Action):
     """Find optimal arbitrage opportunity for a list sorted trades.
 
@@ -145,12 +141,13 @@ class Trader(Action):
 
     async def on_next(self, data):
         trader_address = data.trader_address()
-        balance_pre = await get_balance(data.weth(), data.web3(), trader_address)
+        balance_checker = BalanceChecker(data.web3(), data.weth())
+        balance_pre = await balance_checker.check_total(data.trader_address())
 
         if not perform_trade(data, trader_address):
             raise Exception("No trade performed")
 
-        balance_post = await get_balance(data.weth(), data.web3(), trader_address)
+        balance_post = await balance_checker.check_total(data.trader_address())
 
         data.profit(balance_post - balance_pre)
         logger.info("Finished trading")
