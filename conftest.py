404987ce384731d5b8da4fc31e22c47b0b6f3184
Alexander Poole <alex.o.poole@gmail.com>
2020-12-16 17:20:55 +0100

    build(docker-compose): Fork ganache node

--- conftest.py
+++ conftest.py
@@ -4,6 +4,7 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption("--web3_server", action="store", default="http://127.0.0.1:7545")
+    parser.addoption("--web3_fork", action="store", default="http://127.0.0.1:8545")
     parser.addoption("--redis_server", action="store", default="127.0.0.1:6379")
     parser.addoption(
         "--run_slow", action="store_true", default=False, help="run slow tests"
