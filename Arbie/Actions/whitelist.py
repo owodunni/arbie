7f97dd6dc97bfa47ead2d0e8b968af94786f0c5e
Alexander Poole <alex.o.poole@gmail.com>
2021-01-21 20:52:04 +0100

    fix(Whitelist): Add config params and logging

--- Arbie/Actions/whitelist.py
+++ Arbie/Actions/whitelist.py
@@ -7,13 +7,18 @@ from Arbie.Services import Coingecko
 class Whitelist(Action):
     """SetUp Trader account for trading.
 
+       Coingecko is used for finding whitelisted tokens it has a limit
+       of 10 requests/second. However in reality it is much lower.
+       The base settings allow for 1 requests every 0.6 seconds
+       is equal to 1.67 req/sec. In total we need to do ~6000 request
+       which will take ~60 min.
     [Settings]
     input:
+        requests: 1
+        delay: 0.6
     output:
         whitelist: whitelist
     """
 
     async def on_next(self, data):
-        # 2 requests every 0.6 seconds is equal to 3,33 req/sec
-        # In total we need to do ~6000 request which will take 30 min
-        data.whitelist(await Coingecko(2, 0.6).coins())  # noqa: WPS432
+        data.whitelist(await Coingecko(1, 0.6).coins())  # noqa: WPS432
