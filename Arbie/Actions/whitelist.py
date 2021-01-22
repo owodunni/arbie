ee886e36665668a6e1211062e89e024bd0c7209e
Alexander Poole <alex.o.poole@gmail.com>
2021-01-22 08:18:19 +0100

    fix(CircuitBreaker): Use a CircuitBreaker to not get disconnected
    From Coingecko as easily

--- Arbie/Actions/whitelist.py
+++ Arbie/Actions/whitelist.py
@@ -10,15 +10,20 @@ class Whitelist(Action):
        Coingecko is used for finding whitelisted tokens it has a limit
        of 10 requests/second. However in reality it is much lower.
        The base settings allow for 1 requests every 0.6 seconds
-       is equal to 1.67 req/sec. In total we need to do ~6000 request
-       which will take ~60 min.
+       is equal to 4 req/sec. In total we need to do ~6000 request
+       which will take ~25 min.
     [Settings]
     input:
-        requests: 1
-        delay: 0.6
+        requests: 4
+        delay: 1
+        retries: 3
+        retrie_delay: 10
     output:
         whitelist: whitelist
     """
 
     async def on_next(self, data):
-        data.whitelist(await Coingecko(1, 0.6).coins())  # noqa: WPS432
+        gecko = Coingecko(
+            data.requests(), data.delay(), data.retries(), data.retrie_delay()
+        )
+        data.whitelist(await gecko.coins())
--- tests/unit/async_helpers_test.py
+++ tests/unit/async_helpers_test.py
@@ -1,9 +1,10 @@
 """Unittest of async_helpers.py."""
 import asyncio
+from unittest.mock import MagicMock
 
 import pytest
 
-from Arbie.async_helpers import async_map
+from Arbie.async_helpers import CircuitBreaker, async_map
 
 
 async def pass_through(some_value):
@@ -21,3 +22,18 @@ async def test_async_map():
 async def test_async_map_chunk():
     res = await async_map(pass_through, range(3), 2)
     assert res == [0, 1, 2]
+
+
+class TestCircuitBreaker(object):
+    def test_safe_call(self):
+        mock = MagicMock(side_effect=[Exception(), 1337])  # noqa: WPS432
+        breaker = CircuitBreaker(2, 0, mock)
+        value = breaker.safe_call()
+        assert value == 1337  # noqa: WPS432
+        assert mock.call_count == 2
+
+    def test_safe_call_raises_after_retrie_count(self):
+        mock = MagicMock(side_effect=ValueError())  # noqa: WPS432
+        breaker = CircuitBreaker(1, 0, mock)
+        with pytest.raises(ValueError):
+            breaker.safe_call()
